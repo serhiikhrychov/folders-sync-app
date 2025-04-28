fce54dbefd332c8150dcda7a8d90cb4f280206b3
Serhii Khrychov <serhii.khrychov@un.org>
2025-04-28 20:50:17 +0200

    add sync app part2

---
+++ folders_sync/Logger.cs
@@ -0,0 +1,23 @@
+﻿namespace folders_sync;
+
+public class Logger
+{
+    private static string _logFilePath;
+
+    public Logger(string logFilePath)
+    {
+        _logFilePath = logFilePath;
+        if (!File.Exists(_logFilePath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(_logFilePath));
+        }
+    }
+
+    public void Log(string message)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        string logMessageLine = $"[{timestamp}] {message}";
+        Console.WriteLine(logMessageLine);
+        File.AppendAllText(_logFilePath, logMessageLine + Environment.NewLine);
+    }
+}
